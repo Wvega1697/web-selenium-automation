57fa1cfa8d8dfbddb9f15b891315d0bc2ccd2f9e
WVEGA1697 <williamsv1697@gmail.com>
2020-08-15 11:01:51 -0600

    Fin primera clase

---
+++ SeleniumAutomation/src/test/java/com/javabasic/DataTypes.java
@@ -0,0 +1,29 @@
+package com.javabasic;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Datos Numericos/ byte x; short y; int z; long a;
+		 * 
+		 * x=127; y=32767; z = 2147483647; a = 9223372036854775807L;
+		 * 
+		 * System.out.println(x); System.out.println(y);//
+		 */
+
+		/* Datos Numericos Reales */
+//		float base;
+//		double altura;
+//
+//		base = 30.5F;
+//		altura = 40.76;
+//
+//		System.out.println(base);
+//		System.out.println(altura);
+		
+		char letra1;
+		char letra2;
+	}
+
+}
